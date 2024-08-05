06b99df334efd3bb40e4808ade16fdae9ff5d911
Hollyys <crossrunway01@gmail.com>
2024-08-05 17:18:00 +0900

    240805: initial benchmark

--- C/thread/benchmark.c
+++ C/thread/benchmark.c
@@ -6,3 +6,33 @@
 #include <time.h>
 
 #define COUNT 1000000
+
+void *func(void *arg);
+
+void benchmark(int THREAD_COUNT){
+    pthread_t thread[THREAD_COUNT];
+    void *result;
+
+    for(int i=0; i<THREAD_COUNT; i++){
+        if(pthread_create((thread[i]), NULL, func, (void *)i) != 0){
+            printf("THREAD CREATION FAILED\n");
+            exit(1);
+        }
+    }
+
+    for(int i=0; i<THREAD_COUNT; i++){
+        if(pthread_join(thread[i], &result) != 0){
+            printf("Join Thread Fail\n")
+        }
+    }
+}
+
+int main(){
+    benchmark(5);
+    return 0;
+}
+
+void *func(void *arg){
+    number = *((int*)(&arg));
+    printf("[Thread(%d)] is running.\n", number);
+}
