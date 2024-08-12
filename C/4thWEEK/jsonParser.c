7dd666f7b608afe238c28ae3853ac05f5eb28ba2
Hollyys <crossrunway01@gmail.com>
2024-08-12 13:36:02 +0900

    240812: thread 에러 로직 수정

--- C/4thWEEK/jsonParser.c
+++ C/4thWEEK/jsonParser.c
@@ -16,23 +16,19 @@ struct Setting{
 int main(){
     struct Setting setting;
 
-    JSON_Value *rootValue;
-    JSON_Object *rootObject;
-    JSON_Array *thread_array = json_object_get_array(rootObject, "thread");
-
-    rootValue = json_parse_file("jparser.json");
-    rootObject = json_value_get_object(rootValue);
-
+    JSON_Value *rootValue = json_parse_file("jparser.json");
+    JSON_Object *rootObject = json_value_get_object(rootValue);
     setting.repeat = (int)json_object_get_number(rootObject, "repeat");
     setting.thread_num = (int)json_object_get_number(rootObject, "thread_num");
+    JSON_Array *threadArray = json_object_get_array(rootObject, "thread");
 
     printf("\n** JSON PARSING RESULT **\n");
     printf("Repeat: %d\n", setting.repeat);
     printf("Thread Num: %d\n", setting.thread_num);
     printf("Thread:\n");
     for(int i=0; i<setting.thread_num; i++){
-        JSON_Object *thread_object = json_array_get_object(thread_array, i);
-        char *thread_name = json_array_append_string(thread_object, "name");
+        JSON_Object *threadObject = json_array_get_object(threadArray, i);
+        const char *thread_name = json_object_get_string(threadObject, "name");
         printf("    Name: %s\n", thread_name);
     }
 
