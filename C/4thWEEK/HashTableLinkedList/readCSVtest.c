21cb431d12a2ddd30ccfcf97480a6eb36c4b258f
Hollyys <crossrunway01@gmail.com>
2024-08-13 13:25:51 +0900

    240813: csv파싱 소스 완성

--- C/4thWEEK/HashTableLinkedList/readCSVtest.c
+++ C/4thWEEK/HashTableLinkedList/readCSVtest.c
@@ -13,23 +13,11 @@ int main() {
 
     char buffer[BUFFER_SIZE]; // 버퍼 선언
     while (fgets(buffer, BUFFER_SIZE, file)) { // 파일에서 줄을 읽기
-        int cnt = 0;
-        char *token = strtok(buffer, ","); // 첫 번째 토큰 가져오기
-        char *hash_key, *data;
-        if(token){
-            cnt++;
-            strcpy(hash_key, token);
-        }
-        while (token) {
-            token = strtok(NULL, ","); // 다음 토큰 가져오기
-            if(token){
-                cnt++;
-                strcpy(data, token);
-            }
-        }
+        char *hash_key = strtok(buffer, ",");
+        char *data = strtok(buffer, ",");
+
         printf("\nKey: %s\n", hash_key);
         printf("Data: %s", data);
-        printf(" -> %d\n", cnt);
     }
 
     fclose(file); // 파일 닫기
