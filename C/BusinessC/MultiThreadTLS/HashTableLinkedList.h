ca578068a7b4690b7c65c08e8e06c7db6ff28980
Hollyys <crossrunway@Ubuntu.myguest.virtualbox.org>
2024-08-20 15:08:36 +0900

    240820: 멀티 스레드로 해시 테이블 생성 후 시그널 입력시 출력되는 기능 추가

--- C/BusinessC/MultiThreadTLS/HashTableLinkedList.h
+++ C/BusinessC/MultiThreadTLS/HashTableLinkedList.h
@@ -1,10 +1,10 @@
 #ifndef HASHTABLELINKEDLIST_H
 #define HASHTABLELINKEDLIST_H
 
+#include <pthread.h>
+
 #define BUFFER_SIZE 1024
 #define BUCKET_SIZE 31
-
-extern struct bucket *hashtable;
 
 struct node
 {
@@ -26,5 +26,7 @@ void add(char *hash_key, char *data);
 void remove_key(char *hash_key);
 void search(char *hash_key);
 void display();
+void initialize_table();
+void free_table();
 
 #endif
