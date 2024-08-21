282eb432cbdabb2b2263d87c2c76628821a0e39c
Hollyys <crossrunway@Ubuntu.myguest.virtualbox.org>
2024-08-21 20:04:55 +0900

    240821: 스레드 활용해서 큐에 데이터 적재하는 기능 추가

--- C/BusinessC/Queue/main.c
+++ C/BusinessC/Queue/main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include "queue.h"
+#include "threads.h"
 
 int main()
 {
 	QueueType Q;
 	init(&Q);
 
-	enqueue(&Q, "911 GT3 rs");
-	enqueue(&Q, "BMW M2cs");
-	enqueue(&Q, "Ferrari 458");
-	enqueue(&Q, "Avante N");
+	pthread_t threads[THREAD_NUM];
+	struct ThreadArgs args[THREAD_NUM];
+
+	for (int i = 0; i < THREAD_NUM; i++)
+	{
+		args->Q = &Q;
+		args->data = "911 GT3";
+
+		if (pthread_create(&threads[i], NULL, enqueue_function, (void *)&args[i])) {
+    		fprintf(stderr, "Error creating thread\n");
+        	exit(1);
+		}
+    }
+
+	for (int i = 0; i < THREAD_NUM; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
 
 	display(&Q);
 
