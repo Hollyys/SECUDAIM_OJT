e0b8490934e7e75c56b20dea964703cb3b92edfb
Hollyys <crossrunway@Ubuntu.myguest.virtualbox.org>
2024-08-21 13:59:53 +0900

    240821: 불필요한 코드 및 디렉토리 리팩토링

--- C/BusinessC/MultiThreadTLS/main.c
+++ C/BusinessC/MultiThreadTLS/main.c
@@ -1,13 +1,13 @@
 // main.c
 #include "threads.h"
-#include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <signal.h>
 
 int main() {
 
-	setup_signal_handler();
+	//setup_signal_handler();
+	signal(SIGINT, signal_handler);
 	create_join_thread();
 
     return 0;
