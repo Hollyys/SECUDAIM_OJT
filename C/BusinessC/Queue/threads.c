30d6996ed912d985904bbeaa923d0783e64a0c6b
Hollyys <crossrunway@Ubuntu.myguest.virtualbox.org>
2024-08-22 13:36:56 +0900

    240822: 스헤드를 활용하여 큐에 json에서 읽은 문자열 저장하는 기능 구현

--- C/BusinessC/Queue/threads.c
+++ C/BusinessC/Queue/threads.c
@@ -1,10 +1,16 @@
-#include "threads.h"
 #include "queue.h"
-#include <pthread.h>
+
+extern QueueType Q;
 
 void* enqueue_function(void *arg){
-		struct ThreadArgs *args = (struct ThreadArgs *)arg;
-		enqueue(args->Q, args->data);
+		//struct ThreadArgs *args = (struct ThreadArgs *)arg;
+		JSON_Array *strings = (JSON_Array*)arg;
+
+		for(int i=0; i<json_array_get_count(strings); i++)
+		{
+			const char *string = json_array_get_string(strings, i);
+			enqueue(&Q, (element)string);
+		}
 		
 		return NULL;
 }
