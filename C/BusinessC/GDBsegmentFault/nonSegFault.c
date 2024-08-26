03fe2dd0a53ce415d00ecb29f3beded02a3a2f2d
Hollyys <crossrunway01@gmail.com>
2024-08-26 15:10:49 +0900

    240826: gdb 활룔 예시 코드 구현

---
+++ C/BusinessC/GDBsegmentFault/nonSegFault.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char tmp[10];
+	printf("Enter String: ");
+	scanf("%s", &tmp);
+	printf("tmp: %s\n", tmp);
+	return 0;
+}
